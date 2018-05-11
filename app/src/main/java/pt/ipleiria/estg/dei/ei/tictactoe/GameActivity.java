59e33682b639aa13806a2e959dc8d48bd2588336
Joao Santos <2150666@my.ipleiria.pt>
2018-05-11 15:55:35 +0100

    Initial commit

---
+++ app/src/main/java/pt/ipleiria/estg/dei/ei/tictactoe/GameActivity.java
@@ -0,0 +1,19 @@
+package pt.ipleiria.estg.dei.ei.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.Toast;
+
+public class GameActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+
+
+    }
+}
